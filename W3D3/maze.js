6307fedca76c33a0a9132d44e1288db29f6c37bf
Rebecca <rebeccayilma@gmail.com>
2021-08-04 23:25:04 -0500

    mazeLab

---
+++ W3D3/maze.js
@@ -0,0 +1,28 @@
+var win = true;
+$(function () {
+  $("#maze .boundary").mouseover(red);
+  $("#end").mouseover(end);
+  $("#start").click(reset);
+  $("#maze").mouseleave(leaveMouse);
+});
+function red() {
+  win = false;
+  $(".boundary").addClass("youlose");
+  $("#status").text("You lose! :]").css("color", "red");
+}
+
+function reset() {
+  win = true;
+  $("#status").text("Game Started!").css("color", "green");
+  $(".boundary").each(function () {
+    $(this).removeClass("youlose");
+  });
+}
+
+function end() {
+  let endGame = win == true ? "You win! :]" : "you lost again! :";
+  $("#status").text(endGame);
+}
+function leaveMouse() {
+  red();
+}
