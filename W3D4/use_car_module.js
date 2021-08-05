325c96d9a1360f887f8c9f9302c195020edc6b66
Rebecca <rebeccayilma@gmail.com>
2021-08-05 14:45:07 -0500

    uncomment

--- W3D4/use_car_module.js
+++ W3D4/use_car_module.js
@@ -2,5 +2,5 @@ const car = require('./my_car_module');
 
 console.log(car.drive());
 
-// console.log(car.turn(120));
-// console.log(car.break());
+console.log(car.turn(120));
+console.log(car.break());
